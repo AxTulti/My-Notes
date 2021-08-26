5cc24fe2571b79f95e70e52bcef5bcc29a15d413
Axel <axelyaelrodriguezromero@gmail.com>
2021-08-26 14:56:38 -0500

    Added the CRUD_API_URL to the prod. environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  AUTH_API_URL:'https://my-jwt-auth-api.herokuapp.com'
+  AUTH_API_URL:'https://my-jwt-auth-api.herokuapp.com',
+  CRUD_API_URL: 'https://my-crud-notes.herokuapp.com'
 };
